9d77db10b199f03103a5a6061194d6ca81675729
NataliaJeszke <nataliajeszke8@gmail.com>
2025-04-16 00:08:47 +0200

    system mode for application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import "react-native-reanimated";
+import { useColorScheme } from "react-native";
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
@@ -13,14 +14,23 @@ import { StatusBar } from "expo-status-bar";
 import { useUserStore } from "@/store/userStore";
 
 import { Colors } from "@/constants/Colors";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const { currentTheme } = useUserStore();
+  const { currentTheme, userSelectedTheme, setTheme } = useUserStore();
+  const systemTheme = useColorScheme();
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  useEffect(() => {
+    if (!userSelectedTheme && systemTheme) {
+      setTheme(systemTheme);
+    }
+  }, [userSelectedTheme, systemTheme]);
 
   useEffect(() => {
     if (loaded) {
@@ -33,47 +43,49 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={currentTheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false, animation: "fade" }}
-        />
-        <Stack.Screen
-          name="onboarding"
-          options={{
-            headerShown: false,
-            animation: "fade",
-            presentation: "modal",
-          }}
-        />
-        <Stack.Screen
-          name="spiderForm"
-          options={{
-            presentation: "modal",
-            title: "Dodaj pająka do kolekcji",
-          }}
-        />
-        <Stack.Screen
-          name="favourites"
-          options={{
-            title: "Ulubione pająki",
-            headerBackTitle: "Wstecz",
-            headerTintColor: Colors[currentTheme].tint,
-          }}
-        />
-        <Stack.Screen
-          name="manageModal"
-          options={{
-            presentation: "transparentModal",
-            title: "Wymagana akcja",
-            headerShown: false,
-            contentStyle: { backgroundColor: "transparent" },
-          }}
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={currentTheme === "dark" ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen
+              name="(tabs)"
+              options={{ headerShown: false, animation: "fade" }}
+            />
+            <Stack.Screen
+              name="onboarding"
+              options={{
+                headerShown: false,
+                animation: "fade",
+                presentation: "modal",
+              }}
+            />
+            <Stack.Screen
+              name="spiderForm"
+              options={{
+                presentation: "modal",
+                title: "Dodaj pająka do kolekcji",
+              }}
+            />
+            <Stack.Screen
+              name="favourites"
+              options={{
+                title: "Ulubione pająki",
+                headerBackTitle: "Wstecz",
+                headerTintColor: Colors[currentTheme].tint,
+              }}
+            />
+            <Stack.Screen
+              name="manageModal"
+              options={{
+                presentation: "transparentModal",
+                title: "Wymagana akcja",
+                headerShown: false,
+                contentStyle: { backgroundColor: "transparent" },
+              }}
+            />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
--- store/userStore.ts
+++ store/userStore.ts
@@ -1,31 +1,40 @@
+import { ThemeType } from "@/constants/Colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserState = {
   hasFinishedOnboarding: boolean;
-  toggleHasOnboarded: () => void;
-  currentTheme: "light" | "dark";
+  currentTheme: ThemeType;
+  userSelectedTheme: boolean;
+  setTheme: (theme: ThemeType) => void;
   toggleTheme: () => void;
+  toggleHasOnboarded: () => void;
 };
+
 
 export const useUserStore = create(
   persist<UserState>(
     (set) => ({
       hasFinishedOnboarding: false,
       currentTheme: "light",
-      toggleHasOnboarded: () => {
-        return set((state) => ({
+      userSelectedTheme: false,
+      toggleHasOnboarded: () =>
+        set((state) => ({
           ...state,
           hasFinishedOnboarding: !state.hasFinishedOnboarding,
-        }));
-      },
-      toggleTheme: () => {
-        return set((state) => ({
+        })),
+      toggleTheme: () =>
+        set((state) => ({
           ...state,
           currentTheme: state.currentTheme === "light" ? "dark" : "light",
-        }));
-      },
+          userSelectedTheme: true,
+        })),
+      setTheme: (theme) =>
+        set(() => ({
+          currentTheme: theme,
+          userSelectedTheme: true,
+        })),
     }),
     {
       name: "moltingmate-user-store",
