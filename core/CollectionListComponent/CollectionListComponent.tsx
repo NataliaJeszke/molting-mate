26e24b5ea0476bf38faa8d4d772e158086e7c750
NataliaJeszke <nataliajeszke8@gmail.com>
2025-04-19 18:07:05 +0200

    filters for collection view

--- core/CollectionListComponent/CollectionListComponent.tsx
+++ core/CollectionListComponent/CollectionListComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import { useSpidersStore } from "@/store/spidersStore";
 
@@ -6,13 +6,30 @@ import SpiderFullList from "@/components/commons/SpiderFullList/SpiderFullList";
 import SpiderSectionHeader from "../../components/commons/SpiderSectionHeader/SpiderSectionHeader";
 import { ViewTypes } from "@/constants/ViewTypes.enums";
 import { ScrollView } from "react-native";
+import { useFiltersStore } from "@/store/filtersStore";
 
 const CollectionListComponent = () => {
   const spiders = useSpidersStore((state) => state.spiders);
+  const filters = useFiltersStore((state) => state.filters.collection);
   const viewType = ViewTypes.VIEW_COLLECTION;
-  const collectionSpiders = spiders.map((spider) => ({
-    ...spider,
-  }));
+
+  const filteredSpiders = useMemo(() => {
+    return spiders
+      .filter((spider) => {
+        const matchSpecies = filters.species
+          ? spider.spiderSpecies?.includes(filters.species)
+          : true;
+        const matchAge = filters.age ? spider.age === filters.age : true;
+        const matchGender = filters.gender
+          ? spider.individualType === filters.gender
+          : true;
+
+        return matchSpecies && matchAge && matchGender;
+      })
+      .map((spider) => ({
+        ...spider,
+      }));
+  }, [spiders, filters]);
 
   return (
     <>
@@ -20,9 +37,10 @@ const CollectionListComponent = () => {
         title="Kolekcja"
         spiderCount={spiders.length}
         info="Lista wszystkich pająków"
+        viewType={viewType}
       />
       <ScrollView>
-        <SpiderFullList data={collectionSpiders} viewType={viewType} />
+        <SpiderFullList data={filteredSpiders} viewType={viewType} />
       </ScrollView>
     </>
   );
