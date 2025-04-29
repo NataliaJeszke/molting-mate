ec2b34f8ee37754bb7fb37f66eb36365b6c1907b
NataliaJeszke <nataliajeszke8@gmail.com>
2025-04-29 12:24:16 +0200

    editing feeding, molting, remove works with db

--- store/spidersStore.ts
+++ store/spidersStore.ts
@@ -1,23 +1,11 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
-import { Spider } from "@/models/Spider.model";
-import { db } from "@/db/db";
-import { SpiderDB, spiders } from "@/db/schema";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 import { eq } from "drizzle-orm/sql";
 
-// type SpidersState = {
-//   nextId: number;
-//   spiders: Spider[];
-//   addSpider: (spider: Omit<Spider, "id">) => void;
-//   removeSpider: (spiderId: string) => void;
-//   addToFavorites: (spiderId: string) => void;
-//   removeFromFavorites: (spiderId: string) => void;
-//   updateSpider: (spiderId: string, updates: Partial<Spider>) => void;
-// };
+import { SpiderDB, spiders } from "@/db/schema";
 
 type SpidersState = {
   spiders: SpiderDB[];
@@ -134,55 +122,3 @@ export const useSpidersStore = create(
     },
   ),
 );
-// export const useSpidersStore = create(
-//   persist<SpidersState>(
-//     (set) => ({
-//       spiders: [],
-//       nextId: 1,
-//       addSpider: (spider: Omit<Spider, "id">) =>
-//         set((state) => ({
-//           spiders: [
-//             {
-//               id: String(state.spiders.length + 1),
-//               ...spider,
-//             },
-//             ...state.spiders,
-//           ],
-//         })),
-//       removeSpider: (spiderId: string) => {
-//         return set((state) => {
-//           return {
-//             ...state,
-//             spiders: state.spiders.filter((spider) => spider.id !== spiderId),
-//           };
-//         });
-//       },
-//       addToFavorites: (spiderId: string) =>
-//         set((state) => ({
-//           ...state,
-//           spiders: state.spiders.map((spider) =>
-//             spider.id === spiderId ? { ...spider, isFavourite: true } : spider,
-//           ),
-//         })),
-
-//       removeFromFavorites: (spiderId: string) =>
-//         set((state) => ({
-//           ...state,
-//           spiders: state.spiders.map((spider) =>
-//             spider.id === spiderId ? { ...spider, isFavourite: false } : spider,
-//           ),
-//         })),
-//       updateSpider: (spiderId, updates) =>
-//         set((state) => ({
-//           ...state,
-//           spiders: state.spiders.map((spider) =>
-//             spider.id === spiderId ? { ...spider, ...updates } : spider,
-//           ),
-//         })),
-//     }),
-//     {
-//       name: "moltingmate-spider-store",
-//       storage: createJSONStorage(() => AsyncStorage),
-//     },
-//   ),
-// );
