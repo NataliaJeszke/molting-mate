89e79d96294e47ed337dd88ea6d9b67b6588cfb9
NataliaJeszke <nataliajeszke8@gmail.com>
2025-05-19 19:34:08 +0200

    shadow for cards as in profile styling

--- components/ui/CardComponent.tsx
+++ components/ui/CardComponent.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 
 import { useUserStore } from "@/store/userStore";
-import { Colors } from "@/constants/Colors";
+import { Colors, ThemeType } from "@/constants/Colors";
 
 type CardComponentProps = {
   children: ReactNode;
@@ -14,15 +14,31 @@ const CardComponent = ({ children, customStyle }: CardComponentProps) => {
   const isDarkMode = currentTheme === "dark";
   const cardColors = isDarkMode ? Colors.dark.card : Colors.light.card;
 
-  return <View style={[styles.card, cardColors, customStyle]}>{children}</View>;
+  return (
+    <View style={[styles(currentTheme).card, cardColors, customStyle]}>
+      {children}
+    </View>
+  );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    borderRadius: 12,
-    padding: 16,
-    marginVertical: 8,
-  },
-});
+/* eslint-disable react-native/no-unused-styles */
+const styles = (theme: ThemeType) =>
+  StyleSheet.create({
+    card: {
+      borderRadius: 16,
+      padding: 20,
+      marginVertical: 8,
+      shadowColor: theme === "dark" ? "#000" : "#2e1a47",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: theme === "dark" ? 0.3 : 0.1,
+      shadowRadius: 8,
+      elevation: 4,
+    },
+    sectionTitle: {
+      fontSize: 18,
+      fontWeight: "600",
+      marginBottom: 16,
+      color: theme === "dark" ? "#c9a7f5" : "#2e1a47",
+    },
+  });
 
 export default CardComponent;
